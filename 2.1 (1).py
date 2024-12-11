428e1aeef435186a5e4985a13e40567605d3aa0d
vovandao2005 <vovandao22062005@gmail.com>
2024-12-11 15:47:41 +0700

    Add files via upload

---
+++ 2.1 (1).py
@@ -0,0 +1,13 @@
+print("Vo Van Dao")
+print("235752021610060")
+print("##################")
+############################
+n1=int(input("enter n1 value: "))
+n2=int(input("enter n2 value: "))
+if n1>n2:
+    print("n1 is big")
+elif n1>n2:
+    print("n2 is big")
+else:
+    print("Both numbers are equal")
+          
